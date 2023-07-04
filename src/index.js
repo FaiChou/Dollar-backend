bb5a68de0d1455a0099b0cae22d28fcdc85799d7
FaiChou <faichou.zh@gmail.com>
2023-07-04 14:06:07 +0800

    feat: change to POST

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const cheerio = require('cheerio');
 
 export default {
-  async fetch(request, env, ctx) {
-    console.log(request.url)
-    const {pathname} = new URL(request.url)
-    console.log(pathname)
-    const currency = pathname === '/' ? '%E7%BE%8E%E5%85%83' : pathname;
-    const data = await getExchangeRate(currency);
+  async fetch(request) {
+    if (request.method !== "POST") {
+      return new Response(`Method ${request.method} not allowed.`, {
+        status: 405,
+        headers: {
+          Allow: "POST",
+        },
+      });
+    }
+    const reqBody = await readRequestBody(request);
+    const currency = reqBody.currency || "美元"
+    const type = reqBody.type || "现汇买入价"
+    const data = await getExchangeRate(currency, type);
     const json = JSON.stringify({data}, null, 2);
     return new Response(json, {
       headers: {
@@ -16,14 +23,39 @@ export default {
   },
 };
 
-async function getExchangeRate(currency) {
+async function readRequestBody(request) {
+  const contentType = request.headers.get("content-type");
+  if (contentType.includes("application/json")) {
+    return await request.json();
+  } else if (contentType.includes("application/text")) {
+    return request.text();
+  } else if (contentType.includes("text/html")) {
+    return request.text();
+  } else if (contentType.includes("form")) {
+    const formData = await request.formData();
+    const body = {};
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1];
+    }
+    return JSON.stringify(body);
+  } else {
+    // Perhaps some other type of data was submitted in the form
+    // like an image, or some other binary data.
+    return "a file";
+  }
+}
+
+
+async function getExchangeRate(currency, type = "现汇买入价") {
   const url = 'https://srh.bankofchina.com/search/whpj/search_cn.jsp';
+  const body = "erectDate=&nothing=&pjname=" + encodeURIComponent(currency);
+  console.log(body)
   const response = await fetch(url, {
+      body,
       method: 'POST',
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: `erectDate=&nothing=&pjname=${currency}`
+      }
   });
   const html = await response.text();
   const $ = cheerio.load(html);
@@ -31,7 +63,7 @@ async function getExchangeRate(currency) {
   const headers = table.find('tr:first-child th');
   let targetIndex = -1;
   headers.each((index, element) => {
-      if ($(element).text().includes('现汇买入价')) {
+      if ($(element).text().includes(type)) {
           targetIndex = index;
           return false; // Break the loop
       }
